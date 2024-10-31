5d8aca0c600d83dab5d45078e839a1707bf49f15
lucasberitich <lucasberitich@gmail.com>
2024-10-31 16:39:55 -0300

    Se agregaron tosdos los formularios a utilizar. Faltan modificarlos segun el uso. Agregar conexion a sql y metodos .

---
+++ ProyectoGestionAcademica/Frondend/Form3_Alumnos/Form3_DashBoardAlumnos_5_Informacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoGestionAcademica.Frondend
+{
+    public partial class Form3_DashBoardAlumnos_5_Informacion : Form
+    {
+        public Form3_DashBoardAlumnos_5_Informacion()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ProyectoGestionAcademica/Frondend/Form4_Carreras/Form4_DashBoardCarreras_1_Agregar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoGestionAcademica.Frondend.Form4_Carreras
+{
+    public partial class Form4_DashBoardCarreras_1_Agregar : Form
+    {
+        public Form4_DashBoardCarreras_1_Agregar()
+        {
+            InitializeComponent();
+        }
+    }
+}
